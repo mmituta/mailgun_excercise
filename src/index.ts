c0b8e31e1482ecf6c3a3b6197a0fed50d03e9a44
maciek <S5ndomierz>
2022-04-18 17:35:06 +0200

    Add mailgun verification

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,20 @@ import {
   APIGatewayProxyResult
 } from "aws-lambda";
 import { MailgunArchive } from "./mailgun-archive";
+import { MailgunRequestVerifier } from "./mailgun-verifier";
 import { SNSPublisher } from "./sns-publisher";
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   console.log(event.body);
+  const verifier = new MailgunRequestVerifier();
   const archive: MailgunArchive = new MailgunArchive();
   await archive.store(event.body);
 
   const publisher: SNSPublisher = new SNSPublisher();
   await publisher.publish();
-  
+
   return {
     statusCode: 200,
     body: `Test`
